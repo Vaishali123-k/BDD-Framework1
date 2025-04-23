527b4e86febd00544fb5a33584a82cb83e1d5b2e
Vaishali <Vaishali@LAPTOP-R3ON7IAM>
2025-04-23 06:28:55 +0530

    BDD Framework is created

---
+++ src/main/java/BDDFrameWorkGitHub/BDDFrameWorkGitHub/App.java
@@ -0,0 +1,13 @@
+package BDDFrameWorkGitHub.BDDFrameWorkGitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
